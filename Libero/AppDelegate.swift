6bd666db881319e622cc0dca79abb53c5f5d99d1
John Kotz <john.lyme@mac.com>
2016-09-30 17:31:54 -0400

    Added connection to Heroku Parse server

--- Libero/AppDelegate.swift
+++ Libero/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+            $0.applicationId = "vparU3ObgnU7Fl9mQMwKGrI0GaK1kXfcFKq7UpXS"
+            $0.clientKey = ""
+            $0.server = "https://libero-parse.herokuapp.com/parse"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
