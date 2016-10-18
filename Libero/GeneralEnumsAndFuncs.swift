6c29f368f3073560b1beb54b5b209f921ae07e77
John Kotz <john.lyme@mac.com>
2016-10-18 14:00:41 -0400

    Switched from Enum to class for errors

--- Libero/GeneralEnumsAndFuncs.swift
+++ Libero/GeneralEnumsAndFuncs.swift
@@ -8,19 +8,73 @@
 
 import Foundation
 
-enum BackendError {
-    enum User {
-        enum SignUp {
-            case UsernameExists
-            case PasswordInvalid
+//enum BackendError: Error {
+//    enum User: Error {
+//        enum SignUp: Error {
+//            case UsernameExists
+//            case InvalidUsername
+//        }
+//        enum Login: Error {
+//            case UnknownLogin
+//        }
+//    }
+//    case FailedToConnect
+//    case FailedToSave
+//    case DataDoesntExist
+//    case UnknownError
+//}
+
+// I started with the enumeration method above, but I really wanted subclassing
+
+class BackendError: Error, CustomStringConvertible {
+    class User: BackendError {
+        static let UnknownUserError = User("unknown_user_error")
+        class SignUp: BackendError.User {
+            static let UsernameExists: SignUp = SignUp("username_exists")
+            static let InvalidUsername: SignUp = SignUp("invalid_username")
+            
+            override init(_ val: String) {
+                super.init("signup-\(val)")
+            }
         }
-        enum Login {
-            case UnknownLogin
+        class Login: BackendError.User {
+            static let UnknownLogin = Login("unknown_login")
+            static let UnknownLoginError = Login("unknown_error")
+            
+            override init(_ val: String) {
+                super.init("login-\(val)")
+            }
+        }
+        class PasswordReset: BackendError.User {
+            static let UnknownPasswordResetError = PasswordReset("unknown_error (we should figure out what the error codes are)")
+            
+            override init(_ val: String) {
+                super.init("passwdReset-\(val)")
+            }
+        }
+        
+        override init(_ val: String) {
+            super.init("user-\(val)")
         }
     }
-    case FailedToConnect
-    case FailedToSave
-    case DataDoesntExist
-    case Warrior
-    case Knight
+    class ServerError: BackendError {
+        static let CloudCodeFailed = User("cloud_code_failed")
+        
+        override init(_ val: String) {
+            super.init("signup-\(val)")
+        }
+    }
+    static let FailedToConnect: BackendError = BackendError("failed_to_connect")
+    static let FailedToSave: BackendError = BackendError("failed_to_save")
+    static let DataDoesntExist: BackendError = BackendError("data_doesnt_exist")
+    static let UnknownError: BackendError = BackendError("unknown_error")
+    
+    let raw_value: String
+    init(_ val: String) {
+        self.raw_value = val
+    }
+    
+    var description: String {
+        return "Backend Error: \(raw_value)"
+    }
 }
--- Libero/User.swift
+++ Libero/User.swift
@@ -26,8 +26,9 @@ class User: PFUser {
      Standard login with email and password
      
      - parameter username: The user's username
+     - parameter password: The user's password
      */
-    static func login(withUsername username: String, andPassword password: String, block: @escaping PFUserResultBlock) {
+    static func login(withUsername username: String, andPassword password: String, block: @escaping (User?, BackendError?) -> Void) {
         PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
             if let user = user as? User, error == nil {
                 DispatchQueue.main.async {
@@ -35,7 +36,20 @@ class User: PFUser {
                 }
             }else{
                 DispatchQueue.main.async {
-                    block(nil, error)
+                    if let error = error {
+                        var retError: BackendError! = nil
+                        
+                        switch error._code {
+                        case 205:
+                            retError = BackendError.User.Login.UnknownLogin
+                            break
+                            
+                        default:
+                            retError = BackendError.User.Login.UnknownLogin
+                        }
+                        
+                        block(nil, retError)
+                    }
                 }
             }
         }
@@ -52,14 +66,14 @@ class User: PFUser {
      NOTE: This is untested
      - parameter block: A callback that will be executed on completion (when they login and the modal disapears) with the user and an error if there is one
      */
-    static func loginWithFacebook(block: @escaping PFUserResultBlock) {
+    static func loginWithFacebook(block: @escaping (User?, BackendError?) -> Void) {
         let permissionsArray = ["user_birthday"];
         
         PFFacebookUtils.logIn(withPermissions: permissionsArray) { (user, error) in
             if let user = user as? User, error == nil {
                 block(user, nil)
             }else{
-                block(nil, error)
+                block(nil, BackendError.User.Login.UnknownLoginError)
             }
         }
     }
@@ -71,7 +85,7 @@ class User: PFUser {
      - parameter password: The user's password
      - parameter block: A callback that returns the user and error if there is one
      */
-    static func signUp(withUsername username: String, andPassword password: String, block: @escaping PFUserResultBlock) {
+    static func signUp(withUsername username: String, andPassword password: String, block: @escaping (User?, BackendError?) -> Void) {
         let user = User()
         user.username = username
         user.password = password
@@ -82,18 +96,56 @@ class User: PFUser {
         user.legue = nil
         
         user.signUpInBackground { (success, error) in
-            // TODO: Write the function on the server and put its name here
-            PFCloud.callFunction(inBackground: "", withParameters: ["user": user], block: { (result, error) in
+            if let error = error {
+                var retError: BackendError! = nil
                 
-            })
+                switch error._code {
+                case 125: // Invalid username
+                    retError = BackendError.User.SignUp.InvalidUsername
+                    break
+                    
+                case 202: // The username already exists
+                    retError = BackendError.User.SignUp.UsernameExists
+                    break
+                    
+                default:
+                    retError = BackendError.UnknownError
+                }
+                
+                block(nil, retError)
+                return
+            }else if !success {
+                block(nil, BackendError.UnknownError)
+                return
+            }
+            
+            block(user, nil)
+            
+            // Now if there wasnt an error
+            // TODO: Write the function on the server and put its name here
+//            PFCloud.callFunction(inBackground: "", withParameters: ["user": user], block: { (result, error) in
+//                if result == nil || error != nil {
+//                    block(user, BackendError.ServerError.CloudCodeFailed)
+//                }else{ // TODO: Check the result once we know what it will be
+//                    
+//                }
+//            })
+        }
+    }
+    
+    static func requestPasswordReset(forEmail email:String, block: @escaping (_ error: BackendError.User.PasswordReset?) -> Void) {
+        PFUser.requestPasswordResetForEmail(inBackground: email) { (success, error) in
+            if let error = error {
+                print("Error \(error._code): \(error.localizedDescription)")
+                block(BackendError.User.PasswordReset.UnknownPasswordResetError)
+            }
+            
+            
         }
     }
     
     static var isLoggedIn: Bool {
-        get {
-            return PFUser.current() != nil
-        }
-        set {}
+        return PFUser.current() != nil
     }
     
     static func logout(block: PFUserLogoutResultBlock?) {
