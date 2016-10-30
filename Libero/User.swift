80748dc53dcf912a8785e966c9a8e27f06038108
John Kotz <john.lyme@mac.com>
2016-10-30 15:24:52 -0400

    Adding email verification

--- Libero/User.swift
+++ Libero/User.swift
@@ -222,6 +222,13 @@ class User: PFUser {
         PFUser.logOutInBackground(block: block)
     }
     
+    static func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: email)
+    }
+    
     // ========= End Static Functions ==========
     
     class SurveyResponse {
