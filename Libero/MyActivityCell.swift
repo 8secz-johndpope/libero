7b37057f7a90a7b835e7a0de974cf4ac89c6abc5
Nitasha Kochar <nitashakochar@gmail.com>
2016-11-13 14:21:50 -0500

    made myActivity view

--- Libero/MyActivityCell.swift
+++ Libero/MyActivityCell.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class MyActivityCell: UITableViewCell {
 
+    @IBOutlet weak var activityTitle: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
