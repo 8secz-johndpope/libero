3d121250ec4697314877cb40be8a6ba2cea8ea78
John Kotz <john.lyme@mac.com>
2016-11-15 13:24:00 -0500

    Deselecting in Activity view + slight color change

--- Libero/MyActivityTableViewController.swift
+++ Libero/MyActivityTableViewController.swift
@@ -108,6 +108,10 @@ class MyActivityTableViewController: UITableViewController, DZNEmptyDataSetSourc
         return indexPath
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
